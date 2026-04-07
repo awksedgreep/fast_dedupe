1450a738b95482cdf1a3e47afe7718f0f3d6adb1
Mark Cotner <mark.cotner@gmail.com>
2026-04-07 15:09:45 -0400

    Relax Elixir version requirement to 1.18

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FastDedupe.MixProject do
     [
       app: :fast_dedupe,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       escript: [main_module: FastDedupe.CLI]
